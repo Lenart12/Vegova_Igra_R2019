1391c365e884221f248b3267c65710660453787d
Lenart12 <koslenart@gmail.com>
2019-01-29 20:31:45 +0100

    3d rotation gif

--- Texture/3dtest.py
+++ Texture/3dtest.py
@@ -1,11 +1,32 @@
 from PIL import Image
+import imageio
 
 # gifName = input('Gif: ')
 gifName = 'Texture/ladja.gif'
-# rotation = input('Rotation: ')
-rotation = 280
+try:
+    gif = Image.open(gifName)
+except Exception as e:
+    print(f'Cant open {gifName}: {e}')
+    quit()
 
-SCALE = 20
+rotateGif = input('Rotation gif [Y/n]: ')
+if rotateGif.lower() == ('' or 'y' or 'yes' or '1'):
+    rotateGif = True
+else:
+    rotateGif = False
+    try:
+        rotation = int(input('Rotation (0-360): '))
+    except:
+        print('Cant parse rotation')
+
+SCALE = input('Scale gif [20]: ')
+if SCALE.lower() == (''):
+    SCALE = 20
+else:
+    try:
+        SCALE = int(SCALE)
+    except:
+        print('Cant parse scale')
 
 def getFrames(gif):
     try:
@@ -36,23 +57,31 @@ def makeTransparent(frame):
     frame.putdata(newData)
     return frame
 
-try:
-    gif = Image.open(gifName)
-except Exception as e:
-    print(f'Cant open {gifName}: {e}')
-    quit()
+def render(frames, rotation):
+    w, h = frames[0].width * SCALE, frames[0].height * SCALE
+    dest = Image.new(mode='RGBA', size=(w + len(frames) * SCALE, h + len(frames) * SCALE) )
+    for i in range(len(frames)):
+        f = frames[i]
+        f = makeTransparent(f)
+        f = f.resize((w, h)).rotate(rotation)
+        for offset in range(SCALE):
+            destR = (int(dest.width/2 - w/2), int((dest.height - i*SCALE) - offset - h))
+            dest.paste(f, destR, f)
 
+    return dest
 
 frames = getFrames(gif)
-w, h = frames[0].width * SCALE, frames[0].height * SCALE
-dest = Image.new(mode='RGBA', size=(w + len(frames) * SCALE, h + len(frames) * SCALE) )
 
-for i in range(len(frames)):
-    f = frames[i]
-    f = makeTransparent(f)
-    f = f.resize((w, h)).rotate(rotation)
-    for offset in range(SCALE):
-        destR = (int(dest.width/2 - w/2), int((dest.height - i*SCALE) - offset - h))
-        dest.paste(f, destR, f)
-
-dest.show()
+if rotateGif:
+    gifFrames = []
+    for rotation in range(360):
+        frame = render(frames, rotation)
+        frame.save('/tmp/tmp.png')
+        gifFrames.append(imageio.imread('/tmp/tmp.png'))
+        print(f'Progress {rotation}/360 ({str(rotation*100/360)[:4]}%)')
+    
+    destf = gifName.replace('.', '') + '_rotation.gif'
+    imageio.mimsave(destf, gifFrames)
+    print( f'Saving to {destf}' )
+else:
+    render(frames, rotation).show()
